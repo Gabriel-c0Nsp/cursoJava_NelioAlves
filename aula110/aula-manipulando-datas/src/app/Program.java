f66cc262e4b5860bce5a8c7417ac4b99267ee8e2
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2023-11-05 19:33:57 -0300

    Conteúdo atualizado da aula 110 sobre datas em Java

--- aula110/aula-manipulando-datas/src/app/Program.java
+++ aula110/aula-manipulando-datas/src/app/Program.java
@@ -3,6 +3,7 @@ package app;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class Program {
@@ -34,7 +35,6 @@ public class Program {
     System.out.println("data07 = " + data07);
 
     // Texto no formato customizado
-    
     LocalDate data08 = LocalDate.parse("20/11/2023", fmt01);
     LocalDateTime data09 = LocalDateTime.parse("20/11/2023 11:23", fmt02);
     LocalDate data10 = LocalDate.parse("21/12/2024", DateTimeFormatter.ofPattern("dd/MM/yyyy")); // outra forma de fazer a instanciação
@@ -44,6 +44,26 @@ public class Program {
     System.out.println("data09 = " + data09);
     System.out.println("data10 = " + data10);
     System.out.println("data11 = " + data11);
+
+    // Formatando datas
+    LocalDate data12 = LocalDate.parse("2023-11-21");
+
+    DateTimeFormatter fmt03 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    DateTimeFormatter fmt04 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+    DateTimeFormatter fmt05 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
+    DateTimeFormatter fmt06 = DateTimeFormatter.ISO_DATE_TIME;
+    DateTimeFormatter fmt07 = DateTimeFormatter.ISO_INSTANT;
+
+    System.out.println("data12 = " + data12.format(fmt03));
+    System.out.println("data12 = " + fmt03.format(data12)); // outra forma de chamar
+    System.out.println("data12 = " + data12.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))); // outra forma de chamar
+
+    System.out.println("data05 = " + data05.format(fmt03));
+    System.out.println("data05 = " + data05.format(fmt04));
+
+    System.out.println("data06 = " + fmt05.format(data06));
+    System.out.println("data05 = " + data05.format(fmt06));
+    System.out.println("data06 = " + fmt07.format(data06));
 
   }
 }
